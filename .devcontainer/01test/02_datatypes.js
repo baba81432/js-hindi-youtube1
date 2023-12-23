4aaf515a7cbe9b10ba031dd07d80ce01918246a0
baba81432 <154618908+baba81432@users.noreply.github.com>
2023-12-23 05:08:47 +0000

    datatypes

---
+++ .devcontainer/01test/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) 
+// we are using node js not browser
+console.log(3
+    +3)// code redability should be high
+console.log("baba");
+let name="baba"
+let age=23
+let isLoggedIn=false
+let state
+// number
+// bigInt
+// string
+// boolean
+// null==>stand alone value
+// undefined ==>
+// symbol ===>unique
+
+//non premitive==>object
+console.log(typeof undefined);
---
+++ .devcontainer/01test/o1_variables.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="baba@gmail.com"
+var accountPassword="babashaik"
+accountCity="jaipur"
+
+
+// accountId=23
+accountEmail="babaaa@gamil.com"
+accountPassword="ass"
+let accountState
+/*
+prefer not to use var because of functional issue
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+console.log(accountId)
